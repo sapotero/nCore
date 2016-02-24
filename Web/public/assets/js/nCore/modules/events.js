d678bb7ade3ab57665916c37d392d50b39d7a75d
Сапожников Егор <esapozhnikov@n-core.ru>
2016-02-24 13:32:13 +0300

    new froala && promise loading

--- Web/public/assets/js/nCore/modules/events.js
+++ Web/public/assets/js/nCore/modules/events.js
@@ -688,58 +688,60 @@ nCore.events = (function () {
 
       for (var i = 0; i < data.length; i++) {
         console.log( 'data[i] ',data[i] );
-        var cell = table.rows[data[i].rowIndex].cells[data[i].cellIndex];
-        
-        switch( typeof( data[i].value ) ){
-          case 'object':
-            console.log('value type: Object')
-            
-            // проверяем что за объект
-            switch( data[i].value.constructor ){
-              case Array:
-                cell.textContent = data[i].value.join(', ');
-                break;
-              case Object:
-                cell.textContent = JSON.stringify( data[i].value );
-                break;
-              case Date:
-                cell.textContent = data[i].value;
-                break;
-              default:
-                cell.textContent = data[i].value;
-                break;
-            };
+        // если есть cellIndex, rowIndex, value
+        if ( data[i].hasOwnProperty('cellIndex') && data[i].hasOwnProperty('rowIndex') && data[i].hasOwnProperty('value') ) {
+          var cell = table.rows[data[i].rowIndex].cells[data[i].cellIndex];
+          switch( typeof( data[i].value ) ){
+            case 'object':
+              console.log('value type: Object')
+              
+              // проверяем что за объект
+              switch( data[i].value.constructor ){
+                case Array:
+                  cell.textContent = data[i].value.join(', ');
+                  break;
+                case Object:
+                  cell.textContent = JSON.stringify( data[i].value );
+                  break;
+                case Date:
+                  cell.textContent = data[i].value;
+                  break;
+                default:
+                  cell.textContent = data[i].value;
+                  break;
+              };
 
-            break;
-          case 'string':
-            console.log('value type: String')
-            // для массива
-            // var test = Array(2).fill( data[i].value );
-            // var df = new DocumentFragment(),
-            //     root = document.createElement('div');
-            //     // root.style.border = '1px solid red';
-            // for (var s = 0; s < test.length; s++) {
-            //   var el = document.createElement('div');
-            //   el.style.display = 'block';
-            //   el.style.width   = '100%';
-            //   el.classList.add('tableBlock')
-            //   el.textContent = test[s];
-            //   df.appendChild( el );
-            // };
-            // root.appendChild( df );
-            // cell.textContent = '';
-            // cell.appendChild( root );
-            cell.textContent = data[i].value;
+              break;
+            case 'string':
+              console.log('value type: String')
+              // для массива
+              // var test = Array(2).fill( data[i].value );
+              // var df = new DocumentFragment(),
+              //     root = document.createElement('div');
+              //     // root.style.border = '1px solid red';
+              // for (var s = 0; s < test.length; s++) {
+              //   var el = document.createElement('div');
+              //   el.style.display = 'block';
+              //   el.style.width   = '100%';
+              //   el.classList.add('tableBlock')
+              //   el.textContent = test[s];
+              //   df.appendChild( el );
+              // };
+              // root.appendChild( df );
+              // cell.textContent = '';
+              // cell.appendChild( root );
+              cell.textContent = data[i].value;
 
-            break;
-          case 'number':
-            console.log('value type: Number')
-            cell.textContent = data[i].value;
-            break;
-          default:
-            console.error('value type: DEFAULT', data[i].value, typeof( data[i].value ) )
-            cell.textContent = data[i].value;
-            break;
+              break;
+            case 'number':
+              console.log('value type: Number')
+              cell.textContent = data[i].value;
+              break;
+            default:
+              console.error('value type: DEFAULT', data[i].value, typeof( data[i].value ) )
+              cell.textContent = data[i].value;
+              break;
+          };
         };
       }
 
