990d85e46d51211ae3362718c43a665b3001a41a
Sapotero <egokok@gmail.com>
2016-04-07 14:48:54 +0300

    worker improve

--- assets/js/core/worker.js
+++ assets/js/core/worker.js
@@ -28,14 +28,38 @@ onmessage = function(e) {
     request.send( this.data );
   };
 
+  var Template = function () {
+  }
+  Template.request = Request;
+
+  Template.prototype.load = function(tmp) {
+    console.log('tmp', tmp);
+    return new this.request({
+      type : 'get',
+      url  : '/'+ tmp
+    }).send(
+      function ( response ) {
+        console.log( 'WORKER REQUEST: ', response );
+      },
+      function ( error ) {
+        throw new Error(error);
+      }
+    );
+  };
+
   var Command = function(config) {
     this.action = config.action || '';
     this.data   = config.data   || '';
     return this;
   }
+  Command.prototype.template = Template;
+
   Command.prototype.do = function() {
     if (this.action !== '') {
-      
+      var tmp = this.action.split(':');
+      var module = tmp[0],
+          action = tmp[1];
+      this[module][action].call(this, this.data);
     };
   };
 
@@ -43,18 +67,6 @@ onmessage = function(e) {
     action : e.data[0],
     data   : e.data[1]
   }).do();
-
-  new Request({
-    type : 'get',
-    url  : '/'
-  }).send(
-    function ( response ) {
-      console.log( 'WORKER REQUEST: ', response );
-    },
-    function ( error ) {
-      throw new Error(error);
-    }
-  );
 
   // postMessage( e.data );
 };
